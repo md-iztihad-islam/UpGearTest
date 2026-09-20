ba2e999ff0db19156ebf77b60ec141531617599a
Md. Iztihad Islam <iiztihad@gmail.com>
2026-09-20 23:54:44 +0600

    Test

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,18 +1,31 @@
 // import { defineConfig } from 'vite'
 // import path from "path"
+// import fs from "fs"
 // import react from '@vitejs/plugin-react'
 // import tailwindcss from '@tailwindcss/vite'
 // import sitemapPlugin from 'vite-plugin-sitemap'
+
+// const outDir = path.resolve(__dirname, 'dist')
 
 // // https://vite.dev/config/
 // export default defineConfig({
 //     plugins: [
 //         react(),
 //         tailwindcss(),
+//         {
+//             name: 'ensure-outdir-exists',
+//             closeBundle() {
+//                 fs.mkdirSync(outDir, { recursive: true })
+//             },
+//         },
 //         sitemapPlugin({
 //             hostname: 'https://upgearbd.com',
+//             outDir,
 //         }),
 //     ],
+//     build: {
+//         outDir,
+//     },
 //     resolve: {
 //         alias: {
 //             "@": path.resolve(__dirname, "./src"),
@@ -30,7 +43,6 @@ import sitemapPlugin from 'vite-plugin-sitemap'
 
 const outDir = path.resolve(__dirname, 'dist')
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
@@ -48,6 +60,7 @@ export default defineConfig({
     ],
     build: {
         outDir,
+        target: ['es2018', 'safari12', 'chrome70'],
     },
     resolve: {
         alias: {
